24f591a3798eb3bb4a383e2987dcff2c1ebf9560
arif <arifurrahmanarif223@gmail.com>
2026-03-11 14:47:26 +0600

    Update .gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,14 @@
-// @ts-check
-
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
-import tseslint from 'typescript-eslint';
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
-
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
-  {
-    rules:{
-    }
-  }
+// @ts-check
+
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  {
+    rules:{
+    }
+  }
 );
